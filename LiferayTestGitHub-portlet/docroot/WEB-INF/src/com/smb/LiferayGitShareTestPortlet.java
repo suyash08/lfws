f3c043f55fcfc1dec8b20db46e6d1c482117fb71
suyash08 <suyash08@gmail.com>
2015-04-21 15:44:48 +0530

    Initial commit

---
+++ LiferayTestGitHub-portlet/docroot/WEB-INF/src/com/smb/LiferayGitShareTestPortlet.java
@@ -0,0 +1,11 @@
+package com.smb;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class LiferayGitShareTestPortlet
+ */
+public class LiferayGitShareTestPortlet extends MVCPortlet {
+ 
+
+}
